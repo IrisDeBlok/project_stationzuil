02ffc28982f4390d6454d54fbfe2f5ec9bdd393f
IrisDeBlok <99642080+IrisDeBlok@users.noreply.github.com>
2023-09-18 12:55:49 +0200

    readme

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-x = '412' + '389'
+# input()
 
-print(x)
+# print('Enter Your Name')
+# print(input())
+
+x = input('Enter your name:')
+mijnlijst = ['Uw bericht', x]
+
+for word in mijnlijst:
+    print(input(word))
+
+if len(x) == 0:
+    print('Hallo, anoniem')
+else:
+    print('Hello, ' + x)
